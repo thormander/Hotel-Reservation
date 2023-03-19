a4b0108ac43fa84d5afd0b12883de8c090f64f30
Thor <71967190+thormander@users.noreply.github.com>
2023-03-19 18:18:52 -0500

    removed comment on logout servlet

--- src/main/java/com/hotel/Account/AccountHandler.java
+++ src/main/java/com/hotel/Account/AccountHandler.java
@@ -445,24 +445,3 @@ public class AccountHandler extends HttpServlet {
 	}
 
 }
-
-
-/* this is the code that was on logout servlet(currently unused)
- * public class logoutServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    public logoutServlet() {
-        super();
-    }
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		doGet(request, response);
-	}
-
-}
- * */
