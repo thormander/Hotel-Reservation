d450714f87d7979998a5c0bcc2ba0456e8955c28
Thor <71967190+thormander@users.noreply.github.com>
2023-03-31 18:54:55 -0500

    small updates to login test cases

--- src/main/java/tests/account/Login_Guest.java
+++ src/main/java/tests/account/Login_Guest.java
@@ -19,7 +19,7 @@ class Login_Guest {
         RequestDispatcher dispatcher = Mockito.mock(RequestDispatcher.class);
 
         // set the request parameters for a successful sign in (disclaimer: blank fields are error handled through .jsp)
-        Mockito.when(request.getParameter("username")).thenReturn("unitTestGuest@test.com"); //removal of @test.com should result in failure
+        Mockito.when(request.getParameter("username")).thenReturn("loginTestGuest@test.com"); //removal of @test.com should result in failure
         Mockito.when(request.getParameter("password")).thenReturn("passwordTest");
 
         // mock the request dispatcher to capture the forwarded request
@@ -38,3 +38,10 @@ class Login_Guest {
 /* ON SUCCESS: this means test case received the 'success' from the method for a normal execution of the function (and user is present on database)
  * ON FAIL: means the login credentials were not in the correct format
  * */
+
+
+
+/* Please add these queries to the DB so the login functionality can be tested
+INSERT INTO `hotel`.`account` (`user_name`, `email_id`, `password`, `type`) VALUES ('junitGuestLogin', 'loginTestGuest@test.com', 'passwordTest', 'guest');
+INSERT INTO `hotel`.`account` (`user_name`, `email_id`, `password`, `type`) VALUES ('junitClerkLogin', 'loginTestClerk@test.com', 'passwordTest', 'clerk');
+ */
